f7bf976ea0b7ee56e065b3ee55ed16e28f1221b5
rohit rai <rohitrai2948@gmail.com>
2024-12-07 23:07:10 +0530

    Added nav-bar compononents

--- src/components/navigation/NavigationAction-AddServer.tsx
+++ src/components/navigation/NavigationAction-AddServer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Plus } from "lucide-react";
-import { ActionTooltip } from "../action-tooltips";
+import { ActionTooltip } from "../ActionTooltips";
 import { ModalType, useModal } from "@/hooks/UseModalStore";
 
 export const NavigationActionAddServer = () => {
@@ -12,9 +12,9 @@ export const NavigationActionAddServer = () => {
       <ActionTooltip side="right" align="center" label="Add a Server" sideOffset={16}>
         <div className="listItemWrapper peer">
           <div className="" onClick={() => OpenModal(ModalType.ADD_SERVER)}>
-            <div className="wrapper relative w-[3rem] h-[3rem] flex items-center justify-center text-[#23a55a] group-hover/listItem:text-primary
-              rounded-full group-hover/listItem:rounded-2xl bg-[#313338] group-hover/listItem:bg-[#23a55a]"> 
-                <Plus className="min-h-[2rem] min-w-[2rem]"/>
+            <div className="wrapper relative w-[3rem] h-[3rem] flex items-center justify-center rounded-full group-hover/listItem:rounded-2xl
+            group-hover/listItem:bg-[#23a55a] group-hover/listItem:text-[#fff] text-[#23a55a] bg-white dark:bg-[#313338]"> 
+                <Plus className="min-h-6 min-w-6"/>
             </div>
           </div>
         </div>
--- src/components/navigation/NavigationItem.tsx
+++ src/components/navigation/NavigationItem.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import { ActionTooltip } from "@/components/action-tooltips";
+import { ActionTooltip } from "@/components/ActionTooltips";
 import Link from "next/link";
 import { FaDiscord } from "react-icons/fa6";
 import { useParams, useRouter } from "next/navigation";
@@ -28,7 +28,7 @@ export const NavigationItem = ({ id, image, label }: NavigationItem) => {
             <div
               className={cn(
                 "wrapper relative w-[3rem] h-[3rem] flex items-center justify-center bg-[#313338] rounded-full overflow-hidden group-hover/listItem:rounded-2xl cursor-pointer",
-                params.serverId === id
+                params?.serverId === id
                   ? "bg-primary text-primary-foreground rounded-2xl"
                   : ""
               )}
@@ -47,9 +47,9 @@ export const NavigationItem = ({ id, image, label }: NavigationItem) => {
           <span
             className={cn(
               "rounded-e-sm block w-2 -ml-1 opacity-1 h-6",
-              params.serverId === id
+              params?.serverId === id
                 ? "h-10 bg-primary rounded-2xl"
-                : "group-hover/listItem:bg-red-600"
+                : "group-hover/listItem:bg-[#060607] dark:group-hover/listItem:bg-[#f2f3f5]"
             )}
           ></span>
         </div>
